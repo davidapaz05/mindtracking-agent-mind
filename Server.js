afe289c511369e89a8dcf3f2935b5b70f655e77f
davidapaz05 <davipaz125@gmail.com>
2025-09-19 10:17:48 -0300

    alterando o proxy do cors

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3000', 'https://interface-agente-mind.vercel.app'],
+  origin: ['http://localhost:3001', 'http://localhost:3000', '52.1.59.190'],
   credentials: true,
   methods: 'POST',
   allowedHeaders: ['Content-Type', 'Authorization']
